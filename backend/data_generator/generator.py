0b8c0e54ad2f8c78ae2f23a802824958425d9e0b
Kuba-Bigaj <kbigaj2002@gmail.com>
2025-04-16 10:21:49 +0200

    Added a birthdate generation algorith (untested!)

--- backend/data_generator/generator.py
+++ backend/data_generator/generator.py
@@ -1,11 +1,14 @@
 import random
+
+
 class Generator:
     def __init__(self, input_file):
         # parse data and fill the internal data store
+        print(f"Reading {input_file}...")
         self.values = ["value1", "value2"]
-
-        self.iterator=0
+        self.iterator = 0
+        print(f"Dataset {input_file} initialized!")
 
     def generate_value(self):
-        self.iterator = random.randint(0,len(self.values)-1)
+        self.iterator = random.randint(0, len(self.values) - 1)
         return self.values[self.iterator]
--- backend/data_generator/person.py
+++ backend/data_generator/person.py
@@ -1,13 +1,15 @@
+from datetime import datetime
+
 from data_generator.generator import Generator
 import random
 class Person:
     # these should be initialized only once in the program lifetime
-    print("Initializing data...")
+    print("Initializing Person data sets...")
     NAME_GENERATOR_MALE = Generator("names_male.csv")
     NAME_GENERATOR_FEMALE = Generator("names_female.csv")
     SURNAME_GENERATOR_MALE = Generator("surnames_male.csv")
     SURNAME_GENERATOR_FEMALE = Generator("surnames_female.csv")
-    print("Data initialized!")
+    print("Person data initialized!")
 
     def __init__(self):
         # generate data
@@ -19,6 +21,34 @@ class Person:
             self.gender = "female"
             self.name = self.NAME_GENERATOR_FEMALE.generate_value()
             self.surname = self.SURNAME_GENERATOR_FEMALE.generate_value()
-
+        self.birth_date = Person.generate_random_birthdate()
+        # TODO: PESEL generation
         self.PESEL = "123456789"
+
+    @staticmethod
+    def generate_random_birthdate():
+        """todo: documentation"""
+        age_average = 41.9
+        age_std_deviation = 18.0
+
+        current_date = datetime.today()
+        age_deviation = random.gauss(mu=0, sigma=age_std_deviation)
+
+        birth_year = int(current_date.year - age_average - age_deviation)
+
+        # value validation
+        if birth_year < current_date.year - 120:
+            birth_year = current_date.year - 120
+        elif birth_year > current_date.year - 10:
+            birth_year = current_date.year - 10
+
+        birth_month = random.randint(1, 12)
+        birth_day = random.randint(1, 28)  # good enough
+
+        try:
+            birth_date = datetime(birth_year, birth_month, birth_day).date()
+            return birth_date
+        except ValueError:
+            print(f"Error occurred while generating a date! Data:{birth_year}-{birth_month}-{birth_day}. Attempting to regenerate...")
+            return Person.generate_random_birthdate()
 
